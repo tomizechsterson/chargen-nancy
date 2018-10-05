2b918ac5729bd8f38a05b2620610a2ab48fab1cd
Tomas <tomizechsterson@gmail.com>
2018-10-05 09:26:59 -0700

    Add cors to dd35 and a bit of cleanup in Startup.cs files

--- chargen-nancy-dd35/Startup.cs
+++ chargen-nancy-dd35/Startup.cs
@@ -12,15 +12,26 @@ namespace chargen_nancy_dd35
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o =>
+            {
+                o.AddPolicy("SpecificOrigin", builder =>
+                {
+                    builder.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
+                });
+                o.AddPolicy("AnyOrigin", builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
+            
+            app.UseCors("AnyOrigin");
 
             app.UseOwin(x => x.UseNancy());
             new SqliteDbSetup("characters").CreateTables();
